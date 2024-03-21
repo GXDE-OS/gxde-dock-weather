c08022fc2ec5ad5262a77aa8eb876bcb0821d8bd
gfdgd_xi <3025613752@qq.com>
2024-03-21 13:16:48 +0800

    新增翻译

--- HTYWeather_zh.ts
+++ HTYWeather_zh.ts
@@ -6,12 +6,12 @@
     <message>
         <location filename="forcastwidget.cpp" line="43"/>
         <source>City</source>
-        <translation type="unfinished"></translation>
+        <translation>城市</translation>
     </message>
     <message>
         <location filename="forcastwidget.cpp" line="47"/>
         <source>01-01 Mon</source>
-        <translation type="unfinished"></translation>
+        <translation>01-01 星期一</translation>
     </message>
 </context>
 <context>
@@ -19,69 +19,69 @@
     <message>
         <location filename="weatherplugin.cpp" line="111"/>
         <source>About</source>
-        <translation type="unfinished"></translation>
+        <translation>关于</translation>
     </message>
     <message>
         <location filename="weatherplugin.cpp" line="117"/>
         <location filename="weatherplugin.cpp" line="223"/>
         <source>Set</source>
-        <translation type="unfinished"></translation>
+        <translation>设置</translation>
     </message>
     <message>
         <location filename="weatherplugin.cpp" line="123"/>
         <source>Refresh</source>
-        <translation type="unfinished"></translation>
+        <translation>刷新</translation>
     </message>
     <message>
         <location filename="weatherplugin.cpp" line="129"/>
         <location filename="weatherplugin.cpp" line="185"/>
         <source>Clouds Map</source>
-        <translation type="unfinished"></translation>
+        <translation>地图</translation>
     </message>
     <message>
         <location filename="weatherplugin.cpp" line="135"/>
         <source>Log</source>
-        <translation type="unfinished"></translation>
+        <translation>日志</translation>
     </message>
     <message>
         <location filename="weatherplugin.cpp" line="227"/>
         <source>City</source>
-        <translation type="unfinished"></translation>
+        <translation>城市</translation>
     </message>
     <message>
         <location filename="weatherplugin.cpp" line="236"/>
         <source>Country</source>
-        <translation type="unfinished"></translation>
+        <translation>国家</translation>
     </message>
     <message>
         <location filename="weatherplugin.cpp" line="248"/>
         <source>Search your city and country in &lt;a style=&apos;color:white;&apos; href=&apos;https://openweathermap.org&apos;&gt;openweathermap.org&lt;/a&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>在 &lt;a style=&apos;color:white;&apos; href=&apos;https://openweathermap.org&apos;&gt;openweathermap.org&lt;/a&gt; 搜索城市</translation>
     </message>
     <message>
         <location filename="weatherplugin.cpp" line="278"/>
         <source>Error</source>
-        <translation type="unfinished"></translation>
+        <translation>错误</translation>
     </message>
     <message>
         <location filename="weatherplugin.cpp" line="278"/>
         <source> does not exists !</source>
-        <translation type="unfinished"></translation>
+        <translation> 不存在！</translation>
     </message>
     <message>
         <location filename="weatherplugin.cpp" line="287"/>
         <source>Temperature Unit</source>
-        <translation type="unfinished"></translation>
+        <translation>温度单位</translation>
     </message>
     <message>
         <location filename="weatherplugin.cpp" line="295"/>
         <source>Confirm</source>
-        <translation type="unfinished"></translation>
+        <translation>配置</translation>
     </message>
     <message>
         <location filename="weatherplugin.cpp" line="296"/>
         <source>Cancel</source>
-        <translation type="unfinished"></translation>
+        <translation>取消</translation>
     </message>
 </context>
 </TS>
